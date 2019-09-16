408b63c368e9ee30ce61f977f9337795fbf02b37
sony <olama_18@hotmail.com>
2019-09-16 16:34:06 +0100

    updates

--- src/js/scriptQuery.js
+++ src/js/scriptQuery.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
 
-// //   // Scroll down
-// //   $(function(){
-// //     $(document).on('click','#btn-landing',function () {
-
-// //         const aboutme = $('#section-aboutme').position().top;
-// //         $('html, body').animate({
-// //             scrollTop: aboutme
-// //         }, 700);
-// //     })
-// //  });
-
-
     // // // Scroll top
     // $(function () {
         var upArrow = $('#upArrow').hide();
@@ -23,24 +11,11 @@ $(document).ready(function () {
             // console.log("offset:" + $('#contact').offset().top)
             if (scrollTop > $('#contact').offset().top) {
 
-                upArrow.show();
-              
-                // // smooth scroll top
-                // upArrow.click(function () {
-
-                //     $('html,body').animate({
-                //         scrollTop: 0
-                //     })
-                // })
-
+                upArrow.show();              
+           
             } else {
                upArrow.hide();
             }
         }) //window scroll listener
-    // })  
-
-
-
-
-
+    
 }) //document load 
